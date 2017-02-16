598d0b7b6e79e753a7ca1ac129833c3c697f7f2e
5-5-C <jmarshall.brock@gmail.com>
2017-02-16 16:06:53 -0500

    step 2 & 3 complete using the done function

--- ajax.js
+++ ajax.js
@@ -1,5 +1,35 @@
 $(document).ready(function () {
-
-  /* Your code goes here */
-
+$("#ajax-request").on('click', function() {
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'test'
+  });
 });
+
+
+$("#pingpong-button").on('click', function() {
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/ping',
+    method:'GET',
+    data: 'text'
+  }).done(function (responseData) {
+    console.log('Ajax request done');
+  $('step3456').append(responseData);
+  });
+});
+});
+
+
+
+
+
+
+
+// test
+// $.ajax({
+//   url: 'http://first-ajax-api.herokuapp.com/',
+//   method: 'GET',
+//   data: 'test'  ,
+//   // dataType:
+// });
